098e18ecf10c643fca32d88547ead42b6913877b
Vladimir Ivanov <ivasin75@mail.ru>
2020-10-07 01:10:54 +0300

    add all

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -49,5 +49,6 @@ public class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+    public int getAge(){ return this.age; }
 
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,15 +1,65 @@
-import org.testng.annotations.Test;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class EmployeeTest {
 
-        @Test
-        public void getFirsName() {
-            Employee emp = new Employee();
+    @Before
+    public void addEmployee() {
+        List<Employee> list = new ArrayList<>();
+        Employee emp = new Employee(1, "Inav", "Petrov", "RU", 25);
+        list.add(emp);
+        Employee emp1 = new Employee(2, "John", "Smith", "USA", 23);
+        list.add(emp1);
+    }
+
+    @Test
+    public void getFirstName() {
+        List<Employee> list = new ArrayList<>();
+        Employee emp = new Employee();
+        for (Employee e : list) {
             String i = emp.getFirstName();
             String expected = "Inav";
             assertEquals(i, expected);
         }
+    }
+    @Test
+       public void SubstringMatcher() {
+        List<Employee> list = new ArrayList<>();
+        Employee emp = new Employee();
+        for (Employee e : list) {
+           MatcherAssert.assertThat("n", CoreMatchers.containsString(e.firstName));
+
+        }
+    }
+    @Test
+    public void getFirstNameMatcher(){
+        List<Employee> list = new ArrayList<>();
+        Employee emp = new Employee();
+        for (Employee e : list) {
+            String i = emp.getFirstName();
+            String expected = "Inav";
+            MatcherAssert.assertThat(i, CoreMatchers.equalTo(expected));
+        }
+    }
+
+   @Test
+   public void idEqualsIndexOf() {
+       List<Employee> list = new ArrayList<>();
+       Employee emp = new Employee();
+       Assert.assertEquals(list.size(), emp.id);
+   }
+
+
 }
+
 
